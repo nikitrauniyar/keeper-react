40aaeed62ac0de5f56961ac9b59a4c569d21dd36
Nikit Rauniyar <rauniyarnikit@gmail.com>
2021-11-28 20:52:09 +1100

    Updates

---
+++ precache-manifest.e540ff868587732582fdfa93591e0065.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f57ecda4fe60a7d260f25860312e3e",
+    "url": "/keeper-react/index.html"
+  },
+  {
+    "revision": "a6f56906094e9cf80d55",
+    "url": "/keeper-react/static/js/2.20fe2584.chunk.js"
+  },
+  {
+    "revision": "c716256e69e88a4cb566",
+    "url": "/keeper-react/static/js/main.410206d7.chunk.js"
+  },
+  {
+    "revision": "5d8127b5349a2d8edec3",
+    "url": "/keeper-react/static/js/runtime-main.98eb535c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4119ebf60aa7ff721cdd860b97f8f2b4.js"
+  "/keeper-react/precache-manifest.e540ff868587732582fdfa93591e0065.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keeper-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

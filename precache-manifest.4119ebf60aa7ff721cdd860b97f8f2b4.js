35c8e4700e617ad5df269334e778e41b0568aa4a
Nikit Rauniyar <rauniyarnikit@gmail.com>
2021-11-28 20:50:30 +1100

    Updates

---
+++ precache-manifest.4119ebf60aa7ff721cdd860b97f8f2b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcea727e7fb56819e276a2ec2f3da92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f56906094e9cf80d55",
+    "url": "/static/js/2.20fe2584.chunk.js"
+  },
+  {
+    "revision": "c716256e69e88a4cb566",
+    "url": "/static/js/main.410206d7.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
